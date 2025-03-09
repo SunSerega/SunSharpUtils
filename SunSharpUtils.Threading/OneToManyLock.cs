e9413688d5036edc8b99cb838601a769f64cb8ab
Sun Serega <sunserega2@gmail.com>
2025-03-09 18:13:42 +0100

    Add reworked OneToManyLock

---
+++ SunSharpUtils.Threading/OneToManyLock.cs
@@ -0,0 +1,230 @@
+﻿using System;
+
+using System.Threading;
+
+using System.Runtime.CompilerServices;
+
+namespace SunSharpUtils.Threading;
+
+/// <summary>
+/// Can be unlocked, or locked in one of two modes: One or Many                     <br/>
+/// 1. One: Works like a regular lock, with only one thread allowed at a time       <br/>
+/// 2. Many: Any number of threads allowed, but all of them must be in Many mode    <br/>
+/// <br/>
+/// One mode can be given priority - in that case new threads in Many mode will not queue up until all threads in One mode have finished executing
+/// </summary>
+public sealed class OneToManyLock()
+{
+    private readonly Object sync_lock = new();
+
+    // Set when not locked
+    private readonly ManualResetEventSlim one_wh = new(true);
+    private readonly ManualResetEventSlim many_wh = new(true);
+
+    // How many threads currently have the lock
+    private volatile Int32 doing_one = 0;
+    private volatile Int32 doing_many = 0;
+    // Side A can execute if at any moment doing_B is zero, after doing_A has been set to non-zero
+
+    // *LockedState.Begin methods:
+    // - Not the constructor, because .End needs to be called even if .Begin fails midway
+
+    #region One
+
+    /// <summary>
+    /// </summary>
+    public struct OneLockedState(OneToManyLock root) : IDisposable
+    {
+        private Boolean is_locked = false;
+        private Boolean need_dec = false;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private readonly void Inc()
+        {
+            Interlocked.Increment(ref root.doing_one);
+            root.one_wh.Reset();
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private readonly void Dec()
+        {
+            if (0 != Interlocked.Decrement(ref root.doing_one))
+                return;
+            root.one_wh.Set();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void BeginWithPriority()
+        {
+            Inc();
+            need_dec = true;
+
+            while (true)
+            {
+                if (root.doing_many == 0)
+                    break;
+                root.many_wh.Wait();
+            }
+
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void BeginWithoutPriority()
+        {
+
+            while (true)
+            {
+                Inc();
+                if (root.doing_many == 0)
+                    break;
+                Dec();
+                root.many_wh.Wait();
+            }
+
+            need_dec = true;
+        }
+
+        /// <summary>
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Begin(Boolean with_priority)
+        {
+            if (is_locked)
+                throw new InvalidOperationException();
+
+            if (with_priority)
+                BeginWithPriority();
+            else
+                BeginWithoutPriority();
+
+            Monitor.Enter(root.sync_lock);
+            is_locked = true;
+        }
+
+        /// <summary>
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void End()
+        {
+            if (need_dec)
+            {
+                Dec();
+                need_dec = false;
+            }
+
+            if (is_locked)
+            {
+                Monitor.Exit(root.sync_lock);
+                is_locked = false;
+            }
+        }
+        void IDisposable.Dispose() => End();
+
+    }
+    /// <summary>
+    /// </summary>
+    public OneLockedState NewOneState() => new(this);
+
+    /// <summary>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T OneLocked<T>(Func<T> act, Boolean with_priority)
+    {
+        using var state = NewOneState();
+        state.Begin(with_priority);
+        return act();
+    }
+
+    /// <summary>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void OneLocked(Action act, Boolean with_priority)
+    {
+        using var state = NewOneState();
+        state.Begin(with_priority);
+        act();
+    }
+
+    #endregion
+
+    #region Many
+
+    /// <summary>
+    /// </summary>
+    public struct ManyLockedState(OneToManyLock root) : IDisposable
+    {
+        private Boolean is_locked = false;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private readonly void Inc()
+        {
+            Interlocked.Increment(ref root.doing_many);
+            root.many_wh.Reset();
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private readonly void Dec()
+        {
+            if (0 != Interlocked.Decrement(ref root.doing_many))
+                return;
+            root.many_wh.Set();
+        }
+
+        /// <summary>
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Begin()
+        {
+            if (is_locked)
+                throw new InvalidOperationException();
+
+            while (true)
+            {
+                Inc();
+                if (root.doing_one == 0)
+                    break;
+                Dec();
+                root.one_wh.Wait();
+            }
+
+            is_locked = true;
+        }
+
+        /// <summary>
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void End()
+        {
+            if (!is_locked)
+                return;
+            Dec();
+            is_locked = false;
+        }
+        void IDisposable.Dispose() => End();
+
+    }
+    /// <summary>
+    /// </summary>
+    public ManyLockedState NewManyState() => new(this);
+
+    /// <summary>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T ManyLocked<T>(Func<T> act)
+    {
+        using var state = NewManyState();
+        state.Begin();
+        return act();
+    }
+
+    /// <summary>
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void ManyLocked(Action act)
+    {
+        using var state = NewManyState();
+        state.Begin();
+        act();
+    }
+
+    #endregion
+
+}
---
+++ Tests/OneToManyLock_Tests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+using System.Threading;
+
+using System.Collections.Generic;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SunSharpUtils.Threading.Tests;
+
+[TestClass()]
+public class OneToManyLock_Tests
+{
+
+    [TestMethod()]
+    public void VisualStressTest()
+    {
+        var l = new OneToManyLock();
+
+        var one_priority = true;
+
+        var one_thr_c = 3;
+        var many_thr_c = 100;
+
+        var one_rep_c = 10;
+        var many_rep_c = 1000;
+
+        var counter_lock = new Object();
+        var one_counter = 0;
+        var many_counter = 0;
+
+        var threads = new List<Thread>();
+
+        var done_one = 0;
+        var done_many = 0;
+
+        for (var i = 0; i<one_thr_c; ++i)
+        {
+            void one_thr()
+            {
+                Thread.Sleep(1000);
+                for (Int32 i = 0; i<one_rep_c; ++i)
+                {
+                    l.OneLocked(() =>
+                    {
+                        lock (counter_lock)
+                            one_counter++;
+                        Thread.Sleep(100);
+                        lock (counter_lock)
+                            one_counter--;
+                    }, one_priority);
+                    Interlocked.Increment(ref done_one);
+                    Thread.Sleep(1000);
+                }
+            }
+            threads.Add(new Thread(() => Err.Handle(one_thr))
+            {
+                Name=$"One thr [{i}]"
+            });
+        }
+
+        for (var i = 0; i<many_thr_c; ++i)
+        {
+            void many_thr()
+            {
+                Thread.Sleep(1000);
+                for (Int32 i = 0; i<many_rep_c; ++i)
+                {
+                    l.ManyLocked(() =>
+                    {
+                        lock (counter_lock)
+                            many_counter++;
+                        Thread.Sleep(10);
+                        lock (counter_lock)
+                            many_counter--;
+                    });
+                    Interlocked.Increment(ref done_many);
+                    //Thread.Sleep(1000);
+                }
+            }
+            threads.Add(new Thread(() => Err.Handle(many_thr))
+            {
+                Name=$"Many thr [{i}]"
+            });
+        }
+
+        foreach (var thr in threads)
+            thr.Start();
+
+        var win_thr = new Thread(() => Err.Handle(() =>
+        {
+            static UIElement make_otp_line(String name, out TextBlock tb) => new Viewbox
+            {
+                Child = new StackPanel
+                {
+                    Orientation = Orientation.Horizontal,
+                    Children =
+                    {
+                        new TextBlock{ Text = $"{name}: " },
+                        (tb = new TextBlock()),
+                    }
+                }
+            };
+
+            var g = new Grid
+            {
+                Children =
+                {
+                    make_otp_line("tested (one)", out var tb_tested_one),
+                    make_otp_line("tested (many)", out var tb_tested_many),
+                    make_otp_line("one", out var tb_one),
+                    make_otp_line("many", out var tb_many),
+                }
+            };
+            foreach (UIElement child in g.Children)
+            {
+                Grid.SetRow(child, g.RowDefinitions.Count);
+                g.RowDefinitions.Add(new()
+                {
+                    Height=new(1, GridUnitType.Star)
+                });
+            }
+            var w = new Window()
+            {
+                Content = g,
+                WindowState = WindowState.Maximized,
+            };
+
+            new DispatcherTimer(TimeSpan.FromSeconds(1/60d), DispatcherPriority.Normal, (o, e) =>
+            {
+                tb_tested_one.Text = $"{done_one} / {one_thr_c*one_rep_c}";
+                tb_tested_many.Text = $"{done_many} / {many_thr_c*many_rep_c}";
+                tb_one.Text = one_counter.ToString();
+                tb_many.Text = many_counter.ToString();
+            }, w.Dispatcher).Start();
+
+            w.Show();
+            Dispatcher.Run();
+        }));
+        win_thr.SetApartmentState(ApartmentState.STA);
+        win_thr.Start();
+
+        while (true)
+        {
+            threads.RemoveAll(thr => !thr.IsAlive);
+            if (threads.Count == 0)
+                break;
+
+            using var counter_locker = new ObjectLocker(counter_lock);
+            if (one_counter!=0 && many_counter!=0)
+                throw new InvalidOperationException($"one_counter={one_counter} many_counter={many_counter}");
+            if (one_counter>1)
+                throw new InvalidOperationException($"one_counter={one_counter}");
+            if (many_counter>many_thr_c)
+                throw new InvalidOperationException($"one_counter={one_counter} many_thr_c={many_thr_c}");
+
+        }
+
+        Dispatcher.FromThread(win_thr).InvokeShutdown();
+        win_thr.Join();
+    }
+
+}
