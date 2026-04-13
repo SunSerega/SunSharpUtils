f1eb41f693e14af52df800dedfd745576c14db0e
Sun Serega <sunserega2@gmail.com>
2026-04-14 00:42:31 +0200

    MathExt

--- SunSharpUtils.Bin/BinExt.cs
+++ SunSharpUtils.Bin/BinExt.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
-
-using SunSharpUtils.Ext.Linq;
 
 namespace SunSharpUtils.Bin;
 
---
+++ SunSharpUtils/Ext/Math/MathExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SunSharpUtils.Ext.Math;
+
+/// <summary>
+/// </summary>
+public static class MathExt
+{
+
+    /// <summary>
+    /// </summary>
+    public static Boolean InRange<T>(this T x, T a, T b)
+        where T : IComparable<T>
+    {
+        var cmp = Comparer<T>.Default;
+        if (cmp.Compare(x, a) < 0)
+            return false;
+        if (cmp.Compare(x, b) > 0)
+            return false;
+        return true;
+    }
+
+}
