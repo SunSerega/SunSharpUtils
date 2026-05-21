ad9fabd085017dadad04b11075825c17b88f4796
Sun Serega <sunserega2@gmail.com>
2026-05-21 18:35:49 +0200

    Singleton

---
+++ SunSharpUtils/Singleton.cs
@@ -0,0 +1,21 @@
+﻿namespace SunAssistant.Core;
+
+/// <summary>
+/// </summary>
+public interface ISingleton
+{
+    /// <summary>
+    /// </summary>
+    public abstract static ISingleton Instance { get; }
+}
+
+/// <summary>
+/// </summary>
+public abstract class Singleton<TSelf> : ISingleton
+    where TSelf : Singleton<TSelf>, new()
+{
+    /// <summary>
+    /// </summary>
+    public static TSelf Instance { get; } = new();
+    static ISingleton ISingleton.Instance => Instance;
+}
