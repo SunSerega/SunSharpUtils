44c9e165224f13e61218cde1338d2200b195166a
Sun Serega <sunserega2@gmail.com>
2026-07-19 18:43:43 +0200

    Add Ext.Json

---
+++ SunSharpUtils.Ext.Json/JsonExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SunSharpUtils.Ext.Json;
+
+/// <summary>
+/// </summary>
+public static class JsonExt
+{
+
+    /// <summary>
+    /// Writes the JToken to a StringWriter and returns the resulting string
+    /// </summary>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public static String WriteToString(this JToken token)
+    {
+        var sw = new StringWriter();
+        token.WriteTo(new JsonTextWriter(sw));
+        return sw.ToString();
+    }
+
+}
